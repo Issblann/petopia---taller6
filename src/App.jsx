4051edbba6d8d0b21631fb04811cf3ae84243d96
Issblann <anaisabelpp@outlook.com>
2024-10-22 20:53:21 -0500

    agrego componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import { SeccionParticipar } from './components/landing/seccion-3-participar';
 
 function App() {
   return (
     <>
       <h1>Petopia</h1>
+      <SeccionParticipar />
     </>
   );
 }
