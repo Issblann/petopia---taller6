d6dd0074cbe11c63388e3ccadf98a883be290bd1
Nathalia Galvis <karolgalvis10@gmail.com>
2024-10-22 23:48:27 -0500

    Componente Header - NavBar

--- src/components/landing/NavBar.jsx
+++ src/components/landing/NavBar.jsx
@@ -1,23 +1,37 @@
 import React, { useState } from 'react';
-import '../styles/NavBar.css'; 
+import '../../styles/navbar.css';
+import logo from '../../assets/logo.svg';
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
-      <div className="nav-logo">PEPTOPIA</div>
-      <div className={`nav-items ${isOpen && "open"}`}>
-        <a href="#inicio">Inicio</a>
-        <a href="#adoptar">Adoptar</a>
-        <a href="#participar">Participar</a>
-        <a href="#blog">Blog</a>
-        <a href="#sobre-nosotros">Sobre Nosotros</a>
-      </div>
-      <div className="nav-toggle" onClick={() => setIsOpen(!isOpen)}>
-        <span className="bar"></span>
-        <span className="bar"></span>
-        <span className="bar"></span>
+      <div className="navbar-content">
+        <div className="logo">
+          <span className="logo-name">PETOPIA</span>
+          <img src={logo} alt="Logo" />
+        </div>
+
+        <div className={`nav-links ${isOpen ? 'open' : ''}`}>
+          <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Adopta</a></li>
+            <li><a href="#">Participar</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Sobre Nosotros</a></li>
+          </ul>
+        </div>
+
+        <div className="hamburger" onClick={toggleMenu}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
       </div>
     </nav>
   );
