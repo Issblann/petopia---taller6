a4e4301f8e567b35dee0a788d52e215ab483c76c
Issblann <anaisabelpp@outlook.com>
2024-10-23 21:36:53 -0500

    agrego estilos y componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import { NewsLetterSection } from './components/landing/seccion-5-newsletter';
 
 function App() {
   return (
     <>
       <h1>Petopia</h1>
+      <NewsLetterSection />
     </>
   );
 }
