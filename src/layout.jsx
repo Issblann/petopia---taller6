77a385e0af1fa289120120490cc0c930483b2b0a
Issblann <anaisabelpp@outlook.com>
2024-10-24 22:43:11 -0500

    agrego fuente y seccion 5

--- src/layout.jsx
+++ src/layout.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import './styles/layout/styles.css';
 import NavBar from './components/landing/NavBar';
 import { SeccionParticipar } from './components/landing/seccion-3-participar';
-import  Section3  from './components/landing/Section3';
+import Section3 from './components/landing/Section3';
+import { NewsLetterSection } from './components/landing/seccion-5-newsletter';
 
 export const Layout = () => {
   return (
@@ -14,6 +15,7 @@ export const Layout = () => {
       <main>
         <SeccionParticipar />
         <Section3 />
+        <NewsLetterSection />
       </main>
     </>
   );
