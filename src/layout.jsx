f5548b56d08045aa6cfb0846bf0f1a00b55eb753
Issblann <anaisabelpp@outlook.com>
2024-10-24 21:38:35 -0500

    agrego navbar a layout

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import './styles/layout/styles.css';
+import NavBar from './components/landing/NavBar';
 
 export const Layout = () => {
-  return <main></main>;
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+
+      <main></main>
+    </>
+  );
 };
