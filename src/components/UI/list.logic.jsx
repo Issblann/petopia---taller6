577cec5420a3a590a0ea80eed93882394a50a7a7
Issblann <anaisabelpp@outlook.com>
2024-10-26 14:26:50 -0500

    agrego favoritos de gatos (por ahora)

--- src/components/UI/list.logic.jsx
+++ src/components/UI/list.logic.jsx
@@ -6,11 +6,14 @@ import DogService from '../../services/dog/DogService';
 export const ListDogsAndCats = () => {
   const [dogs, setDogs] = useState([]);
   const [cats, setCats] = useState([]);
-
+  const [favouritesCat, setFavouritesCat] = useState([]);
   useEffect(() => {
     const getApis = async () => {
       const cats = await CatService.getCats();
       setCats(cats);
+
+      const favouritesCat = await CatService.getCatFavourites();
+      setFavouritesCat(favouritesCat);
 
       const dogs = await DogService.getDogs();
       setDogs(dogs);
@@ -19,7 +22,31 @@ export const ListDogsAndCats = () => {
     getApis();
   }, []);
 
+  const addFavoriteHandler = async (id) => {
+    try {
+      await CatService.postCatFavourite({
+        image_id: id,
+        sub_id: 'my-user-1234',
+      });
+      // Optionally update favouritesCat state if needed
+      const updatedFavourites = await CatService.getCatFavourites();
+      setFavouritesCat(updatedFavourites);
+    } catch (error) {
+      console.error(
+        'Error adding favorite:',
+        error.response?.data || error.message
+      );
+    }
+  };
+
   //este archivo se cambia, la idea es que haya un componente de logica para cada lista y usar la misma presentacion (UI).
 
-  return <ListPresentation dogs={dogs} cats={cats} />;
+  return (
+    <ListPresentation
+      dogs={dogs}
+      cats={cats}
+      addFavoriteHandler={addFavoriteHandler}
+      favouritesCat={favouritesCat}
+    />
+  );
 };
