ec189d40a785c20bd2d1b993a498f8c1f4f38411
Issblann <anaisabelpp@outlook.com>
2024-10-26 15:57:44 -0500

    ui seccion 1 con estilos y agrego a layout

--- src/layout.jsx
+++ src/layout.jsx
@@ -4,6 +4,7 @@ import NavBar from './components/landing/NavBar';
 import { SeccionParticipar } from './components/landing/seccion-3-participar';
 import Section3 from './components/landing/Section3';
 import { NewsLetterSection } from './components/landing/seccion-5-newsletter';
+import { Seccion1Descubre } from './components/landing/seccion-1-descubre';
 
 export const Layout = () => {
   return (
@@ -13,6 +14,7 @@ export const Layout = () => {
       </header>
 
       <main>
+        <Seccion1Descubre />
         <SeccionParticipar />
         <Section3 />
         <NewsLetterSection />
