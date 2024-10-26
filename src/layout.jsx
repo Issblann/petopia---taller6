7bf965a0e5d558a1f08992eaeeb75c14ed28708e
Issblann <anaisabelpp@outlook.com>
2024-10-26 17:10:06 -0500

    agrego seccion 2 UI y estilos, agrego a layout

--- src/layout.jsx
+++ src/layout.jsx
@@ -4,6 +4,7 @@ import NavBar from './components/landing/NavBar';
 import { SeccionParticipar } from './components/landing/seccion-3-participar';
 import Section3 from './components/landing/Section3';
 import { NewsLetterSection } from './components/landing/seccion-5-newsletter';
+import { SeccionAdopcion } from './components/landing/seccion-2-adopcion';
 
 export const Layout = () => {
   return (
@@ -13,6 +14,7 @@ export const Layout = () => {
       </header>
 
       <main>
+        <SeccionAdopcion />
         <SeccionParticipar />
         <Section3 />
         <NewsLetterSection />
