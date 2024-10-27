3ad8a4cf8677b56ec0f30ef393ae82d1fc473773
Nathalia Galvis <karolgalvis10@gmail.com>
2024-10-27 00:54:36 -0500

    Integracion componente modal

--- src/components/UI/list.logic.jsx
+++ src/components/UI/list.logic.jsx
@@ -1,17 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { ListPresentation } from './list.presentation';
-import CatService from '../../services/cat/CatService';
-import DogService from '../../services/dog/DogService';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { ListPresentation } from "./list.presentation";
+import Modal from "./Modal";
+import CatService from "../../services/cat/CatService";
+import DogService from "../../services/dog/DogService";
+import { useLocation } from "react-router-dom";
 
 export const ListDogsAndCats = () => {
   const location = useLocation();
-  console.log(location.pathname);
   const [data, setData] = useState([]);
   const [favoritesData, setFavoritesData] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedPet, setSelectedPet] = useState(null);
+
   useEffect(() => {
     const getApis = async () => {
-      if (location.pathname === '/gatos') {
+      if (location.pathname === "/gatos") {
         const cats = await CatService.getCats();
         setData(cats);
 
@@ -21,7 +24,7 @@ export const ListDogsAndCats = () => {
         const dogs = await DogService.getDogs();
         setData(dogs);
 
-        const favoritesDog = await CatService.getCatFavorites();
+        const favoritesDog = await DogService.getDogFavorites();
         setFavoritesData(favoritesDog);
       }
     };
@@ -31,35 +34,49 @@ export const ListDogsAndCats = () => {
 
   const addFavoriteHandler = async (id) => {
     try {
-      if (location.pathname === '/gatos') {
+      if (location.pathname === "/gatos") {
         await CatService.postCatFavorite({
           image_id: id,
-          sub_id: 'my-user-1234',
+          sub_id: "my-user-1234",
         });
         const updatedFavoritesCats = await CatService.getCatFavorites();
         setFavoritesData(updatedFavoritesCats);
       } else {
         await DogService.postDogFavorite({
           image_id: id,
-          sub_id: 'my-user-1234',
+          sub_id: "my-user-1234",
         });
         const updatedFavoritesDogs = await DogService.getDogFavorites();
         setFavoritesData(updatedFavoritesDogs);
       }
     } catch (error) {
       console.error(
-        'Error adding favorite:',
+        "Error adding favorite:",
         error.response?.data || error.message
       );
     }
   };
 
+  const openModalHandler = (pet) => {
+    setSelectedPet(pet);
+    setIsModalOpen(true);
+  };
+
+  const closeModalHandler = () => {
+    setIsModalOpen(false);
+    setSelectedPet(null);
+  };
+
   return (
-    <ListPresentation
-      data={data}
-      location={location}
-      addFavoriteHandler={addFavoriteHandler}
-      favoritesData={favoritesData}
-    />
+    <>
+      <ListPresentation
+        data={data}
+        location={location}
+        addFavoriteHandler={addFavoriteHandler}
+        openModalHandler={openModalHandler}
+        favoritesData={favoritesData}
+      />
+      <Modal isOpen={isModalOpen} onClose={closeModalHandler} petDetails={selectedPet} />
+    </>
   );
 };
