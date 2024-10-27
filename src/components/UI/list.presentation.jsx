6dbbe3aa27a717a0da481a424098d3e72ee78aae
Nathalia Galvis <karolgalvis10@gmail.com>
2024-10-27 00:54:22 -0500

    Integracion componente modal

--- src/components/UI/list.presentation.jsx
+++ src/components/UI/list.presentation.jsx
@@ -1,52 +1,44 @@
-import React from 'react';
-import '../../styles/list/styles.css';
+import React from "react";
+import "../../styles/list/styles.css";
+
 export const ListPresentation = ({
   data,
   location,
   addFavoriteHandler,
+  openModalHandler,
   favoritesData,
 }) => {
-  console.log(data, 'api mascotas');
-  console.log(favoritesData, 'favoritos');
   return (
     <section className="list-section">
       <h1 className="title-pet">
-        {location.pathname === '/gatos'
-          ? 'GATOS EN ADOPCIÓN'
-          : 'PERROS EN ADOPCIÓN'}
+        {location.pathname === "/gatos"
+          ? "GATOS EN ADOPCIÓN"
+          : "PERROS EN ADOPCIÓN"}
       </h1>
 
       <ul className="list">
-        {data.map((data) => (
-          <li className="card-list--pet" key={data.id}>
-            <img src={data.url} alt={data.id} />
-
-            {data.breeds.map((breed) => (
+        {data.map((pet) => (
+          <li className="card-list--pet" key={pet.id}>
+            <img src={pet.url} alt={pet.id} />
+            {pet.breeds.map((breed) => (
               <div className="card-list__information" key={breed.id}>
                 <h1>{breed.name}</h1>
                 {breed.temperament && (
                   <div className="card-list__breed-temperament">
                     {breed.temperament
-                      .split(',')
+                      .split(",")
                       .slice(0, 6)
                       .map((temperament) => (
                         <p key={temperament}>{temperament}</p>
                       ))}
                   </div>
                 )}
-
-                <button className="card-list--ver-detalles">
+                <button
+                  className="card-list--ver-detalles"
+                  onClick={() => openModalHandler({ ...pet, name: breed.name, temperament: breed.temperament })}
+                >
                   Ver detalles
                 </button>
-
-                {/* <button
-                  className="card-list--ver-detalles"
-                  onClick={() => {
-                    addFavoriteHandler(data.id), console.log(data.id);
-                  }}
-                >
-                  Agregar a favoritos
-                </button> */}
               </div>
             ))}
           </li>
