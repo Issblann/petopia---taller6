62755091eefb1233aa6ff39fb3795f7005ad56e0
Issblann <anaisabelpp@outlook.com>
2024-10-26 23:07:42 -0500

    agrego favoritos funcionalidad a dog service

--- src/services/dog/DogService.js
+++ src/services/dog/DogService.js
@@ -1,6 +1,7 @@
 import { fetchInterceptor } from '../interceptor/interceptor';
 
-export const URL_BASE = 'https://api.thedogapi.com/v1/images';
+const URL_BASE = 'https://api.thedogapi.com/v1/images';
+const URL_BASE_FAVORITES = 'https://api.thedogapi.com/v1/favourites';
 const GET_DOGS = `${URL_BASE}/search?&page=0&limit=25&has_breeds=true&order=RANDOM`;
 
 class DogService {
@@ -22,6 +23,29 @@ class DogService {
     try {
       const apiKey = import.meta.env.VITE_DOG_API_KEY;
       const res = await fetchInterceptor(apiKey).get(`${URL_BASE}/${id}`);
+      return res.data;
+    } catch (error) {
+      throw new Error(`Status code ${error}`);
+    }
+  }
+
+  async postDogFavorite(favorite) {
+    try {
+      const apiKey = import.meta.env.VITE_DOG_API_KEY;
+      const res = await fetchInterceptor(apiKey).post(
+        URL_BASE_FAVORITES,
+        favorite
+      );
+      return res.data;
+    } catch (error) {
+      throw new Error(`Status code ${error}`);
+    }
+  }
+
+  async getDogFavorites() {
+    try {
+      const apiKey = import.meta.env.VITE_DOG_API_KEY;
+      const res = await fetchInterceptor(apiKey).get(URL_BASE_FAVORITES);
       return res.data;
     } catch (error) {
       throw new Error(`Status code ${error}`);
