91af49303cac1252c03fc121bc98929630feda28
Nathalia Galvis <karolgalvis10@gmail.com>
2024-10-29 00:01:11 -0500

    Add ruta de favoritos

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { ListDogsAndCats } from '../components/UI/list.logic';
 import { Home } from '../components/home';
+import {Favorites} from '../components/UI/favorites';
+
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/gatos" element={<ListDogsAndCats />} />
       <Route path="/perros" element={<ListDogsAndCats />} />
+      <Route path="/gatos/favoritos" element={<Favorites />} />
+      <Route path="/perros/favoritos" element={<Favorites />} />
     </Routes>
   );
 };
