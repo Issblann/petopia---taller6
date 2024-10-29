5525a1f512461f1f60948675750d1a0042e82bc4
Issblann <anaisabelpp@outlook.com>
2024-10-28 21:36:48 -0500

    agrego vectores a layout

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,12 +1,25 @@
 import Footer from './components/landing/footer';
 import NavBar from './components/landing/NavBar';
 import './styles/layout/styles.css';
-
+import SVG1 from './assets/vectors/svg1.svg';
+import SVG2 from './assets/vectors/svg2.svg';
+import SVG3 from './assets/vectors/svg3.svg';
 export const Layout = ({ children }) => {
   return (
     <>
       <NavBar />
-      <main>{children}</main>
+      <main>
+        {children}
+        <div className="svg1">
+          <img src={SVG1} alt="svg1" />
+        </div>
+        <div className="svg2">
+          <img src={SVG2} alt="svg2" />
+        </div>
+        <div className="svg3">
+          <img src={SVG3} alt="svg2" />
+        </div>
+      </main>
       <Footer />
     </>
   );
