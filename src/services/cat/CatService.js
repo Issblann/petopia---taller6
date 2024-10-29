66462916a0f50c1cb6044e6670067b9b18dea5b7
Issblann <anaisabelpp@outlook.com>
2024-10-28 20:04:22 -0500

    adapto servicios para redux

--- src/services/cat/CatService.js
+++ src/services/cat/CatService.js
@@ -2,17 +2,17 @@ import { fetchInterceptor } from '../interceptor/interceptor';
 
 const URL_BASE = 'https://api.thecatapi.com/v1/images';
 const URL_BASE_FAVORITES = 'https://api.thecatapi.com/v1/favourites';
-const GET_CATS = `${URL_BASE}/search?&page=0&limit=25&has_breeds=true&order=RANDOM`;
 
 class CatService {
   constructor() {
-    this.baseUrl = GET_CATS;
+    this.baseUrl = URL_BASE;
   }
 
-  async getCats() {
+  async getCats(page = 0) {
     try {
       const apiKey = import.meta.env.VITE_CAT_API_KEY;
-      const res = await fetchInterceptor(apiKey).get(this.baseUrl);
+      const getCats = `${this.baseUrl}/search?&page=${page}&limit=25&has_breeds=true&order=RANDOM`;
+      const res = await fetchInterceptor(apiKey).get(getCats);
       return res.data;
     } catch (error) {
       throw new Error(`Status code ${error}`);
@@ -28,16 +28,6 @@ class CatService {
       throw new Error(`Status code ${error}`);
     }
   }
-
-  // async getCatBreeds() {
-  //   try {
-  //     const apiKey = import.meta.env.VITE_CAT_API_KEY;
-  //     const res = await fetchInterceptor(apiKey).get('https://api.thecatapi.com/v1/breeds');
-  //     return res.data;
-  //   } catch (error) {
-  //     throw new Error(`Status code ${error}`);
-  //   }
-  // }
 
   async postCatFavorite(favorite) {
     try {
