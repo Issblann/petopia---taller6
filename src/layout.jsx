ef90ddf8c9fc6e1a26271ca6bb3b3879153e31af
Issblann <anaisabelpp@outlook.com>
2024-10-29 23:31:11 -0500

    quito console log

--- src/layout.jsx
+++ src/layout.jsx
@@ -2,7 +2,6 @@ import Footer from './components/landing/footer';
 import NavBar from './components/landing/NavBar';
 import './styles/layout/styles.css';
 export const Layout = ({ children }) => {
-  console.log(location.pathname);
   return (
     <>
       <NavBar />
