0dfeea3566a048063c188cc69654baa8ab40cf8c
Issblann <anaisabelpp@outlook.com>
2024-10-30 00:29:45 -0500

    cambio de ruta deploy

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,7 +4,7 @@ import { SeccionAdopcion } from './landing/seccion-2-adopcion';
 import { SeccionParticipar } from './landing/seccion-3-participar';
 import { SeccionConectando } from './landing/Section-4-conectando';
 import { NewsLetterSection } from './landing/seccion-5-newsletter';
-import { Hero } from './landing/Hero';
+import { Hero } from './landing/hero';
 
 export const Home = () => {
   return (
