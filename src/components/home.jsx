1b5b84aae3fec90286e89247ae9b7c0c5d14ee0b
Issblann <anaisabelpp@outlook.com>
2024-10-30 00:27:32 -0500

    cambio de ruta deploy

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Seccion1Descubre } from './landing/seccion-1-descubre';
 import { SeccionAdopcion } from './landing/seccion-2-adopcion';
 import { SeccionParticipar } from './landing/seccion-3-participar';
-import { SeccionConectando } from './landing/section-4-conectando';
+import { SeccionConectando } from './landing/Section-4-conectando';
 import { NewsLetterSection } from './landing/seccion-5-newsletter';
 import { Hero } from './landing/Hero';
 
