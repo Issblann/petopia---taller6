33df6ce1ecd363b3a13f037265c6622e710c7f4c
Issblann <anaisabelpp@outlook.com>
2024-10-30 00:25:31 -0500

    cambio nombre rutas

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Seccion1Descubre } from './landing/Seccion-1-descubre';
-import { SeccionAdopcion } from './landing/Seccion-2-adopcion';
-import { SeccionParticipar } from './landing/Seccion-3-participar';
-import { SeccionConectando } from './landing/Section-4-conectando';
-import { NewsLetterSection } from './landing/Seccion-5-newsletter';
+import { Seccion1Descubre } from './landing/seccion-1-descubre';
+import { SeccionAdopcion } from './landing/seccion-2-adopcion';
+import { SeccionParticipar } from './landing/seccion-3-participar';
+import { SeccionConectando } from './landing/section-4-conectando';
+import { NewsLetterSection } from './landing/seccion-5-newsletter';
 import { Hero } from './landing/Hero';
 
 export const Home = () => {
@@ -16,5 +16,5 @@ export const Home = () => {
       <SeccionConectando />
       <NewsLetterSection />
     </>
-  );  
+  );
 };
