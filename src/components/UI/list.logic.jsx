1c477b17e60cc77fe89b65fa04133db1b5e149f0
Issblann <anaisabelpp@outlook.com>
2024-10-29 23:32:03 -0500

    manejo de redux

--- src/components/UI/list.logic.jsx
+++ src/components/UI/list.logic.jsx
@@ -1,83 +1,66 @@
 import React, { useEffect, useState } from 'react';
 import { ListPresentation } from './list.presentation';
-import CatService from '../../services/cat/CatService';
-import DogService from '../../services/dog/DogService';
 import { useLocation } from 'react-router-dom';
-import NameService from '../../services/names/NameService';
+import { useDispatch, useSelector } from 'react-redux';
+import { thunks } from '../../redux/slice/animals/thunks';
+import { actions } from '../../redux/slice/animals/slice';
 
 export const ListDogsAndCats = () => {
+  const dispatch = useDispatch();
   const location = useLocation();
-  const [data, setData] = useState([]);
   const [petNames, setPetNames] = useState([]);
-  const [favoritesData, setFavoritesData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
 
+  const {
+    cats,
+    dogs,
+    currentPage,
+    favoritesCats,
+    favoritesDogs,
+    status,
+    cat,
+    dog,
+  } = useSelector((state) => state.animals);
   useEffect(() => {
-    const getApis = async () => {
-      setData([]);
-      setPetNames([]);
-      setFavoritesData([]);
-      setIsLoading(true);
-      try {
-        if (location.pathname === '/gatos') {
-          const cats = await CatService.getCats();
-          setData(cats);
+    if (location.pathname === '/gatos') {
+      dispatch(thunks.fetchCats({ page: currentPage }));
+      dispatch(thunks.fetchFavoritesCats());
+    } else if (location.pathname === '/perros') {
+      dispatch(thunks.fetchDogs({ page: currentPage }));
+      dispatch(thunks.fetchFavoritesDogs());
+    }
+  }, [currentPage, location.pathname]);
 
-          const favoritesCat = await CatService.getCatFavorites();
-          setFavoritesData(favoritesCat);
-        } else if (location.pathname === '/perros') {
-          const dogs = await DogService.getDogs();
-          setData(dogs);
+  const handleNextPage = async () => {
+    dispatch(actions.setPage(currentPage + 1));
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
 
-          const favoritesDog = await DogService.getDogFavorites();
-          setFavoritesData(favoritesDog);
-        }
+  const handlePreviousPage = () => {
+    if (currentPage > 0) dispatch(actions.setPage(currentPage - 1));
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
 
-        const names = await NameService.getNames();
-        setPetNames(names);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    getApis();
-  }, [location.pathname]);
-
-  const addFavoriteHandler = async (id) => {
-    try {
-      if (location.pathname === '/gatos') {
-        await CatService.postCatFavorite({
-          image_id: id,
-          sub_id: 'my-user-1234',
-        });
-        const updatedFavoritesCats = await CatService.getCatFavorites();
-        setFavoritesData(updatedFavoritesCats);
-      } else {
-        await DogService.postDogFavorite({
-          image_id: id,
-          sub_id: 'my-user-1234',
-        });
-        const updatedFavoritesDogs = await DogService.getDogFavorites();
-        setFavoritesData(updatedFavoritesDogs);
-      }
-    } catch (error) {
-      console.error(
-        'Error adding favorite:',
-        error.response?.data || error.message
-      );
+  const getPetById = (id) => {
+    if (location.pathname === '/gatos') {
+      dispatch(thunks.fetchCatById({ id }));
+    } else if (location.pathname === '/perros') {
+      dispatch(thunks.fetchDogById({ id }));
     }
   };
 
   return (
     <ListPresentation
-      data={data}
+      cats={cats}
+      dogs={dogs}
+      status={status}
+      handlePreviousPage={handlePreviousPage}
+      handleNextPage={handleNextPage}
       location={location}
-      addFavoriteHandler={addFavoriteHandler}
-      favoritesData={favoritesData}
+      getPetById={getPetById}
+      currentPage={currentPage}
+      favoritesCats={favoritesCats}
+      favoritesDogs={favoritesDogs}
       petNames={petNames}
-      isLoading={isLoading}
     />
   );
 };
--- src/components/UI/list.presentation.jsx
+++ src/components/UI/list.presentation.jsx
@@ -6,20 +6,31 @@ import {
   PiHeartFill,
 } from 'react-icons/pi';
 import { Modal } from '../../components/UI/Modal';
+import { useNavigate } from 'react-router-dom';
 
 export const ListPresentation = ({
-  data,
+  cats,
+  dogs,
   location,
+  getPetById,
   addFavoriteHandler,
-  favoritesData,
+  handlePreviousPage,
+  handleNextPage,
+  favoritesCats,
+  favoritesDogs,
   petNames,
-  isLoading,
+  currentPage,
+  status,
 }) => {
+  const isCatsPage = location.pathname === '/gatos';
+  const data = isCatsPage ? cats : dogs;
+  const favoritesData = isCatsPage ? favoritesCats : favoritesDogs;
+
+  const navigate = useNavigate();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedPetData, setSelectedPetData] = useState(null);
 
-  const openModal = (petData) => {
-    setSelectedPetData(petData);
+  const openModal = () => {
     setIsModalOpen(true);
   };
 
@@ -28,23 +39,35 @@ export const ListPresentation = ({
     setSelectedPetData(null);
   };
 
-  if (isLoading) {
-    return (
-      <div className="loading">
-        <p>Cargando datos de mascotas...</p>
-      </div>
-    );
-  }
+  const handleRedirect = () => {
+    const path = isCatsPage ? '/gatos/favoritos' : '/perros/favoritos';
+    navigate(path);
+  };
 
   return (
     <section className="list-section">
+      {status === 'loading' && (
+        <div className="loading">
+          <p>Cargando datos de mascotas...</p>
+        </div>
+      )}
+      {status === 'failed' && (
+        <div className="error">
+          <p>Error al traer la data de mascotas</p>
+        </div>
+      )}
+      {data.length === 0 && (
+        <div className="loading">
+          <p>No hay datos de mascotas en este momento</p>
+        </div>
+      )}
       <h1 className="title-pet">
         {location.pathname === '/gatos'
           ? 'GATOS EN ADOPCIÓN'
           : 'PERROS EN ADOPCIÓN'}
       </h1>
       <div className="favorites-button-container">
-        <button className="button-favorites">
+        <button className="button-favorites" onClick={handleRedirect}>
           <PiHeartFill size={35} color="#8645a0" />
           Ver favoritos <span>({favoritesData.length})</span>
         </button>
@@ -74,13 +97,9 @@ export const ListPresentation = ({
 
                   <button
                     className="card-list--ver-detalles"
-                    onClick={() => openModal({ 
-                      id: pet.id, 
-                      url: pet.url, 
-                      name: petName?.firstName, 
-                      breed: breed.name, 
-                      temperament: breed.temperament 
-                    })}
+                    onClick={() => {
+                      openModal(), getPetById(pet.id);
+                    }}
                   >
                     Ver detalles
                   </button>
@@ -103,7 +122,11 @@ export const ListPresentation = ({
       </div>
 
       {/* Aquí renderizamos el modal */}
-      <Modal isOpen={isModalOpen} onClose={closeModal} petDetails={selectedPetData} />
+      <Modal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        petDetails={selectedPetData}
+      />
     </section>
   );
 };
